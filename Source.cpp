6c424f52f8a6ef33b6171782bafb8a45a5e27dd4
DanielSarf <72676273+DanielSarf@users.noreply.github.com>
2022-04-29 16:25:30 +0500

    Adding Assignment Question to comments for easier reference

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,14 @@
+/*Create a class Test.
+Test has 4 data members namely, dm1,dm2, dm3, dm4 of different data types.
+dm1 belongs to Date class, you can choose any data type for other data members.
+dm1 is a static data member and is only changed by calling a function dm1Modify().
+Initial value of dm1 is set to your birthdate.
+dm1Modify() always shifts the date to next date.
+dm1Modify() is called every time a constructor or destructor is called for the class.
+Create appropriate set and get methods for each data member.
+Using the concepts studied in course, enforce the constraint on functions that are not supposed to change data members must not do so even by mistake.*/
+
+
 #include <iostream>
 #include <string>
 
